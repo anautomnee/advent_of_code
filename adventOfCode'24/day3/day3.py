d44196acfe2b7f6e27e47d4ad3f04ab49ce10003
Anastasiia Kozlova <anautomnee@gmail.com>
2024-12-03 10:39:22 +0100

    Task 2 day 3 complete yay

--- adventOfCode'24/day3/day3.py
+++ adventOfCode'24/day3/day3.py
@@ -2,15 +2,23 @@ import re
 from utils import read_input
 
 input = read_input("adventOfCode'24/day3")
-small_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+small_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 def get_multiplication_res(string):
     result = 0
-    regex_incorrupt = "mul\\([0-9]{1,3},[0-9]{1,3}\\)"
+    isEnabled = True
+    regex_incorrupt = "mul\\([0-9]{1,3},[0-9]{1,3}\\)|don't\\(\\)|do\\(\\)"
     incorrupted = re.findall(regex_incorrupt, string)
     for set in incorrupted:
-        numbers = re.findall("[0-9]{1,3}", set)
-        result += int(numbers[0]) * int(numbers[1])
+        if set == "do()":
+            isEnabled = True
+        elif set == "don't()":
+            isEnabled = False
+        else:
+            if not isEnabled:
+                continue
+            numbers = re.findall("[0-9]{1,3}", set)
+            result += int(numbers[0]) * int(numbers[1])
     return result
 
 
