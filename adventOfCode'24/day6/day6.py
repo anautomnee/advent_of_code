befa4b238727863ee265fcf35c86ed4423ad2820
Anastasiia Kozlova <anautomnee@gmail.com>
2024-12-06 21:43:39 +0100

    Task 2 day 6 efficiency on hold

--- adventOfCode'24/day6/day6.py
+++ adventOfCode'24/day6/day6.py
@@ -13,6 +13,8 @@ small_input = """....#.....
 #.........
 ......#..."""
 
+class InfiniteLoopException(Exception):
+    pass
 
 def movement_logic(position, direction, map, positions_array):
     
@@ -30,20 +32,30 @@ def movement_logic(position, direction, map, positions_array):
         "<": "^"
     }
 
+    MAX_ITERATIONS = 10000
+    position_loop_counter = 0
+
     while True:
+        try:
+            position_loop_counter += 1
+            if(position_loop_counter > MAX_ITERATIONS):
+                raise InfiniteLoopException("Potential infinite loop detected!")
 
-        dy, dx = directions[direction]
-        nx, ny = position[0] + dx, position[1] + dy
+            dy, dx = directions[direction]
+            nx, ny = position[0] + dx, position[1] + dy
 
-        if nx >= len(map[0]) or nx < 0 or ny >= len(map) or ny < 0:
-            break
+            if nx >= len(map[0]) or nx < 0 or ny >= len(map) or ny < 0:
+                break
 
-        if map[ny][nx] == '#':
-            direction = turnRight[direction]
-            continue
+            if map[ny][nx] == '#':
+                direction = turnRight[direction]
+                continue
 
-        position = [nx, ny]
-        positions_array.add(tuple(position))
+            position = [nx, ny]
+            positions_array.add(tuple(position))
+        except Exception:
+            return True
+    return False
   
 
 
@@ -60,11 +72,34 @@ def log_path(map_string):
             if j == direction:
                 guard_position = [x, y]
 
+                    
+
     # Movement logic
+    loop_counter = 0
     positions_array = set()
     positions_array.add(tuple(guard_position))
-    movement_logic(guard_position, direction, map, positions_array)
 
-    print(len(positions_array))
+    # Set up obstruction
+    for i, row in enumerate(map):
+        for j, col in enumerate(row):
+            if col != guard_position and col != '#' and col != '^':
+                new_arr = list(map[i])
+                new_arr[j] = '#'
+                map[i] = new_arr
+                res = movement_logic(guard_position, direction, map, positions_array)
+                # if res:
+                #     for row in map:
+                #         print(row, "\n")
+                #     print("-----------")
+                if res:
+                    loop_counter += 1
+                map[i][j] = '.'
+                map[i] = "".join(map[i])
+
+    print(loop_counter)
+
+    #movement_logic(guard_position, direction, map, positions_array, loop_counter)
+
+    #print(len(positions_array))
 
 log_path(input)
