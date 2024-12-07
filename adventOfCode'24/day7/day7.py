3a0d69ce347aaf1681a8c16a476e6813388e1cd0
Anastasiia Kozlova <115838344+anautomnee@users.noreply.github.com>
2024-12-07 08:24:35 +0000

    loop added

--- adventOfCode'24/day7/day7.py
+++ adventOfCode'24/day7/day7.py
@@ -8,9 +8,21 @@ small_input = """190: 10 19
 21037: 9 7 18 13
 292: 11 6 16 20"""
 
+
 def get_calibration(string):
     calibrations = string.split("\n")
     for row in calibrations:
         row = row.split(":")
         res = int(row[0])
         numbers = (row[1].split(" "))
+        operators = ["*"] * (len(numbers)-1)
+        combined_array = []
+        kn, ko = 0
+        for i in range(len(row) + len(operators)):
+            if i%2:
+                combined_array.append(numbers[kn])
+                kn += 1
+            else:
+                combined_array.append(operators[ko])
+                ko += 1
+        print(combined_array)
